fae3f4c071df2a02c5bdc56b91ca30eb8423f999
Felipe Sarmiento <felipesarmiento4851@gmail.com>
2022-04-13 13:29:54 -0500

    Updates

---
+++ precache-manifest.4af17e8279aef118cf3afd33a7eb47f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "171e55b3d844e197a50d1510e0b07d00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "039f6537999972f39cbe",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "039f6537999972f39cbe",
+    "url": "/static/js/main.f84ecea4.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
